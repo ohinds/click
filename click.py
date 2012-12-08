7c1b2dba1bd823ca0e2be1690325fbf6bfa0e696
ohinds <ohinds@gmail.com>
2012-12-08 17:22:57 -0500

    Added support for signatures, silence, and strong beats.

--- click.py
+++ click.py
@@ -1,34 +1,19 @@
 #!/usr/bin/env python
 
 import numpy
-from scipy.io import wavfile
+import ordered_yaml_loader
+import re
 import sys
 import wave
 
 from pylab import *
+from scipy.io import wavfile
 
-_AUDIO_SAMPLE_RATE = 2 # bytes
 _AUDIO_FRAME_RATE = 44100 # hertz
-
-
-def read_map_file(filename):
-    click_map = []
-
-    with open(filename, "r") as mapfile:
-        for line in mapfile:
-            if line[0] == "#":
-                continue
-
-            fields = line.strip().split()
-            click_map.append((int(fields[0]), int(fields[1]), fields[2]))
-
-    return click_map
 
 
 def read_wav_data(wav_filename):
     rate, wav_data = wavfile.read(wav_filename)
-
-    import pdb; pdb.set_trace()
 
     if rate != _AUDIO_FRAME_RATE:
         raise ValueError(
@@ -36,23 +21,22 @@ def read_wav_data(wav_filename):
                                                    _AUDIO_FRAME_RATE))
     return wav_data
 
-
-def write_wav_data(filename, rate, data):
-    wavfile.write(filename, rate, data)
-
 def get_tempo_len(tempo):
     return int((60. / tempo) * _AUDIO_FRAME_RATE)
 
+def parse_signature(sig_str):
+    match = re.match(r'([0-9]+)/([0-9]+)', sig_str)
+    return int(match.groups(0)[0]), int(match.groups(0)[1])
+
 def compute_total_samples(click_map):
     samples = 0
-    beat = click_map[0][0]
-    tempo = click_map[0][1]
 
-    for transition in click_map[1:]:
-        next_beat = transition[0]
-        samples += (next_beat - beat) * get_tempo_len(tempo)
-        beat = next_beat
-        tempo = transition[1]
+    for section in click_map:
+        sec_val = section.values()[0]
+        sig = parse_signature(sec_val["signature"])
+        effective_tempo = sec_val['tempo'] * sig[1] / 4.
+        samples += (sig[0] * sec_val['measures'] *
+                    get_tempo_len(effective_tempo))
 
     return samples
 
@@ -62,29 +46,31 @@ def make_tempo_click(sample, tempo):
     tempo_click.resize(target_len)
     return tempo_click
 
-def create_click_from_map(sample, click_map):
+def create_click_from_map(accent_sample, sample, click_map):
     click_wav = numpy.zeros(compute_total_samples(click_map),
                             numpy.int16)
-    beat = click_map[0][0]
-    tempo = click_map[0][1]
-    on = click_map[0][2] == "on"
 
     start = 0
-    for transition in click_map[1:]:
-        next_beat = transition[0]
+    for section in click_map:
+        sec_val = section.values()[0]
+        sig = parse_signature(sec_val["signature"])
+        effective_tempo = sec_val['tempo'] * sig[1] / 4.
+        off = "silent" in sec_val and sec_val["silent"]
 
-        if on:
-            this_click_wav = make_tempo_click(sample, tempo)
-            for i in xrange(next_beat - beat):
-                click_wav[start:(start+len(this_click_wav))] = this_click_wav
+        if (sig[0] > 1):
+            this_accent_wav = make_tempo_click(accent_sample,
+                                               effective_tempo)
+        this_click_wav = make_tempo_click(sample, effective_tempo)
+        for i in xrange(sec_val['measures']):
+            for j in xrange(sig[0]):
+                if off:
+                    this_wav = numpy.zeros_like(this_click_wav)
+                elif sig[0] != 1 and j == 0:
+                    this_wav = this_accent_wav
+                else:
+                    this_wav = this_click_wav
+                click_wav[start:(start+len(this_wav))] = this_wav
                 start += len(this_click_wav)
-        else:
-            # TODO make empty stuff
-            pass
-
-        beat = next_beat
-        tempo = transition[1]
-        on = transition[2] == "on"
 
     return click_wav
 
@@ -97,15 +83,17 @@ def main(argv):
     out_wav = argv[2]
 
     click_sample = read_wav_data("click.wav")
-    click_map = read_map_file(map_file)
-    click_wav = create_click_from_map(click_sample, click_map)
+    accent_click_sample = read_wav_data("accent_click.wav")
+    click_map = ordered_yaml_loader.load(map_file)
+    click_wav = create_click_from_map(accent_click_sample,
+                                      click_sample, click_map)
 
     #plot(click_wav)
     #show()
 
-    print "writing wav file of %0.2f seconds." % (
-        len(click_wav) / float(_AUDIO_FRAME_RATE))
-    write_wav_data(out_wav, _AUDIO_FRAME_RATE, click_wav)
+    print "writing wav file of %0.2f minutes." % (
+        len(click_wav) / float(_AUDIO_FRAME_RATE) / 60.)
+    wavfile.write(out_wav, _AUDIO_FRAME_RATE, click_wav)
 
 if __name__ == "__main__":
     sys.exit(main(sys.argv))
